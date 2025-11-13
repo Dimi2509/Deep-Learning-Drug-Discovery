e5713872a36a08c8b29240789f7871c7e8ddca8e
Guillermo Quiroga <guillermoquiroga1@hotmail.com>
2025-11-13 14:01:14 +0100

    Adding the Gin Model

--- src/models.py
+++ src/models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn.functional as F
-from torch_geometric.nn import GCNConv, global_mean_pool
+from torch_geometric.nn import GCNConv, global_mean_pool, GINConv
+import torch.nn as nn
 
 
 class GCN(torch.nn.Module):
@@ -22,6 +23,45 @@ class GCN(torch.nn.Module):
         x = global_mean_pool(x, batch)  # [batch_size, hidden_channels]
 
         # 3. Apply a final classifier
+        x = self.linear(x)
+
+        return x
+    
+
+class GIN(torch.nn.Module):
+    def __init__(self, num_node_features, hidden_channels = 64):
+        super(GIN,self).__init__()
+
+        # GIN needs Multi-Layer Perceptron (MLP) for each layer
+
+        mlp1 = nn.Sequential(
+            nn.Linear(num_node_features, hidden_channels),
+            nn.ReLU(),
+            nn.Linear(hidden_channels, hidden_channels)
+        )
+
+        mlp2 = nn.Sequential(
+            nn.Linear(hidden_channels,hidden_channels),
+            nn.ReLU(),
+            nn.Linear(hidden_channels,hidden_channels)
+        )
+
+        #GINConv sums is a wrapper learns from their neighbors
+        self.conv1 = GINConv(mlp1, train_eps=True)
+        self.conv2 = GINConv(mlp2, train_eps=True)
+        self.linear = nn.Linear(hidden_channels,1)
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+
+        #1 Gin layers
+        x = self.conv1(x, edge_index)
+        x = x.relu()
+        x = self.conv2(x,edge_index)
+
+        #2 readout
+        x = global_mean_pool(x, batch)
+
+        #3 Prediction
         x = self.linear(x)
 
         return x
