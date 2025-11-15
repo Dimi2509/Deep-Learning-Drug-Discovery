5e759a78da644550c9ee6d6b11bdc2e4564c8935
Guillermo Quiroga <guillermoquiroga1@hotmail.com>
2025-11-15 17:28:11 +0100

    Best Performance yet with 512 hidden channel, 5MLP and one residual net

--- src/models.py
+++ src/models.py
@@ -29,7 +29,7 @@ class GCN(torch.nn.Module):
     
 
 class GIN(torch.nn.Module):
-    def __init__(self, num_node_features, hidden_channels = 128, dropout = 0.0):
+    def __init__(self, num_node_features, hidden_channels = 512, dropout = 0.0):
         super(GIN,self).__init__()
 
         # GIN needs Multi-Layer Perceptron (MLP) for each layer
@@ -38,10 +38,10 @@ class GIN(torch.nn.Module):
         self.dropout = dropout
 
         # Projection to make the input matches the hidden channels, for residual connection
-        #self.input_proj = nn.Linear(num_node_features, hidden_channels)
+        self.input_proj = nn.Linear(num_node_features, hidden_channels)
 
         mlp1 = nn.Sequential(
-            nn.Linear(num_node_features, hidden_channels),  #If we remove residual connection add num_node_features at the first input 
+            nn.Linear(hidden_channels, hidden_channels),  #If we remove residual connection add num_node_features at the first input 
             nn.ReLU(),
             #nn.Dropout(dropout),
             nn.Linear(hidden_channels, hidden_channels)
@@ -57,21 +57,28 @@ class GIN(torch.nn.Module):
         mlp3 = nn.Sequential(
             nn.Linear(hidden_channels, hidden_channels),
             nn.ReLU(),
-            nn.Dropout(dropout),
+            #nn.Dropout(dropout),
             nn.Linear(hidden_channels,hidden_channels)
         )
 
         mlp4 = nn.Sequential(
             nn.Linear(hidden_channels, hidden_channels),
             nn.ReLU(),
-            nn.Dropout(dropout),
+            #nn.Dropout(dropout),
             nn.Linear(hidden_channels,hidden_channels)
         )
 
         mlp5 = nn.Sequential(
             nn.Linear(hidden_channels, hidden_channels),
             nn.ReLU(),
-            nn.Dropout(dropout),
+            #nn.Dropout(dropout),
+            nn.Linear(hidden_channels,hidden_channels)
+        )
+
+        mlp6 = nn.Sequential(
+            nn.Linear(hidden_channels, hidden_channels),
+            nn.ReLU(),
+            #nn.Dropout(dropout),
             nn.Linear(hidden_channels,hidden_channels)
         )
 
@@ -84,6 +91,7 @@ class GIN(torch.nn.Module):
         self.conv3 = GINConv(mlp3, train_eps=True)
         self.conv4 = GINConv(mlp4, train_eps=True)
         self.conv5 = GINConv(mlp5, train_eps=True)
+        #self.conv6 = GINConv(mlp6, train_eps=True)
 
 
         #Adding BatchNorm
@@ -92,6 +100,7 @@ class GIN(torch.nn.Module):
         self.bn3 = nn.BatchNorm1d(hidden_channels)
         self.bn4 = nn.BatchNorm1d(hidden_channels)
         self.bn5 = nn.BatchNorm1d(hidden_channels)
+        #self.bn6 = nn.BatchNorm1d(hidden_channels)
 
         #Linear Layer
         self.linear = nn.Linear(hidden_channels,1)
@@ -101,15 +110,15 @@ class GIN(torch.nn.Module):
         x, edge_index, batch = data.x, data.edge_index, data.batch
         
         #Input Proyection
-        #x = self.input_proj(x)
+        x = self.input_proj(x)
         
 
         #1 Gin layers
-        #identity = x
+        identity = x
         x = self.conv1(x, edge_index)
         x = self.bn1(x)
         x = x.relu()
-        #x = x + identity  # ← Residual connection
+        x = x + identity  # ← Residual connection
         #x = F.dropout(x, p=self.dropout, training=self.training)
         
         #2 Gin Layers
@@ -117,7 +126,7 @@ class GIN(torch.nn.Module):
         x = self.conv2(x,edge_index)
         x = self.bn2(x)
         x = x.relu()
-        #x = x + identity
+        x = x + identity
         #x = F.dropout(x, p=self.dropout, training=self.training)
 
         #3 Gin Layers
@@ -125,7 +134,7 @@ class GIN(torch.nn.Module):
         x = self.conv3(x, edge_index)
         x = self.bn3(x)
         x = x.relu()
-        #x = x + identity
+        x = x + identity
         #x = F.dropout(x, p=self.dropout, training=self.training)
 
         #4 Gin Layers
@@ -133,7 +142,7 @@ class GIN(torch.nn.Module):
         x = self.conv4(x, edge_index)
         x = self.bn4(x)
         x = x.relu()
-        #x = x + identity
+        x = x + identity
         #x = F.dropout(x, p=self.dropout, training=self.training)
 
         #5 Gin Layers
@@ -141,17 +150,23 @@ class GIN(torch.nn.Module):
         x = self.conv5(x, edge_index)
         x = self.bn5(x)
         x = x.relu()
-        #x = x + identity
+        x = x + identity
         #x = F.dropout(x, p=self.dropout, training=self.training)
+
+        #6 Gin Layer
+        #x = self.conv6(x, edge_index)
+        #x = self.bn6(x)
+        #x = x.relu()
+        #x = x + identity
 
 
 
 
         #2 readout
-        x = global_mean_pool(x, batch)
+        x = global_max_pool(x, batch)
 
         #3 Prediction
-        x = F.dropout(x, p=self.dropout, training=self.training)
+        #x = F.dropout(x, p=self.dropout, training=self.training)
         x = self.linear(x)
 
         return x
